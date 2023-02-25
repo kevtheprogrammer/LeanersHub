c80711643dc4b2805ac66755d31fd4f82d5e84c4
Kelvin Banda <72869357+shazziedev@users.noreply.github.com>
2023-02-25 11:40:35 +0200

    commit 1

---
+++ LearnersHub/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
+from acc.views import HomePage
+
+urlpatterns = [
+    path("",HomePage ,name='home'),
+    path("admin/", admin.site.urls),
+    path("account/",include('acc.urls')),
+    
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
